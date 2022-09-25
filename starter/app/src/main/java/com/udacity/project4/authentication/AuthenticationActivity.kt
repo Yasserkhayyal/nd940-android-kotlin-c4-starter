c8062130a282823876983e740805cb286908a620
yasserkhayyal <yasser.khayal@gmail.com>
2022-09-25 17:53:40 +0200

    Location-Reminders: add sign in flow using FirebaseUI

--- starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/authentication/AuthenticationActivity.kt
@@ -1,24 +1,105 @@
 package com.udacity.project4.authentication
 
+import android.app.Activity
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.IdpResponse
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
+import com.udacity.project4.databinding.ActivityAuthenticationBinding
+import com.udacity.project4.locationreminders.RemindersActivity
 
 /**
  * This class should be the starting point of the app, It asks the users to sign in / register, and redirects the
  * signed in users to the RemindersActivity.
  */
 class AuthenticationActivity : AppCompatActivity() {
+    private lateinit var signInFlowActivityResultLauncher: ActivityResultLauncher<Intent>
+    private lateinit var binding: ActivityAuthenticationBinding
+
+    private val activityResultCallback = ActivityResultCallback<ActivityResult?> {
+        val response = IdpResponse.fromResultIntent(it.data)
+        if (it.resultCode == Activity.RESULT_OK) {
+            // User successfully signed in
+            handleSuccessfulSignIn()
+        } else {
+            // Sign in failed. If response is null the user canceled the
+            // sign-in flow using the back button. Otherwise
+            // response.getError().getErrorCode() is checked and handled.
+            handleFailedSignIn(response)
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_authentication)
-//         TODO: Implement the create account and sign in using FirebaseUI, use sign in using email and sign in using Google
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_authentication)
+        setSupportActionBar(binding.toolbar)
 
-//          TODO: If the user was authenticated, send him to RemindersActivity
+        signInFlowActivityResultLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+            activityResultCallback
+        )
 
-//          TODO: a bonus is to customize the sign in flow to look nice using :
-        //https://github.com/firebase/FirebaseUI-Android/blob/master/auth/README.md#custom-layout
+        binding.loginBtn.setOnClickListener {
+            onLoginButtonClicked()
+        }
+    }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        signInFlowActivityResultLauncher.unregister()
+    }
+
+    private fun launchSignInFlow() {
+        val signInIntent = AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setAvailableProviders(
+                listOf(
+                    AuthUI.IdpConfig.GoogleBuilder().build(),
+                    AuthUI.IdpConfig.EmailBuilder().build()
+                )
+            )
+            .setTheme(R.style.AppTheme).build()
+        signInFlowActivityResultLauncher.launch(signInIntent)
+    }
+
+    private fun handleSuccessfulSignIn() {
+        //navigate to ReminderActivity
+        startActivity(Intent(this, RemindersActivity::class.java))
+        finish()
+    }
+
+    private fun handleFailedSignIn(response: IdpResponse?) {
+        if (response == null) return //user clicked back and canceled the sign in flow
+        AlertDialog.Builder(this@AuthenticationActivity)
+            .setTitle(R.string.error_popup_title).apply {
+                response.error?.message?.let {
+                    setMessage(it)
+                } ?: setMessage(R.string.sign_in_general_error)
+            }.setNegativeButton(R.string.ok_btn) { dialog: DialogInterface, _: Int ->
+                dialog.dismiss()
+            }.setPositiveButton(R.string.retry) { dialog: DialogInterface, _: Int ->
+                launchSignInFlow()
+                dialog.dismiss()
+            }.create().show()
+    }
+
+    private fun onLoginButtonClicked() {
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            //Login is required, start the signIn flow
+            launchSignInFlow()
+        } else {
+            //user is already signed in, route him/her to RemindersActivity
+            handleSuccessfulSignIn()
+        }
     }
 }
--- starter/app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
+++ starter/app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
@@ -1,34 +1,63 @@
 package com.udacity.project4.locationreminders
 
-import android.Manifest
-import android.content.pm.PackageManager
+import android.content.Intent
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
 import com.udacity.project4.R
-import kotlinx.android.synthetic.main.activity_reminders.*
+import com.udacity.project4.authentication.AuthenticationActivity
+import com.udacity.project4.databinding.ActivityRemindersBinding
 
 /**
  * The RemindersActivity that holds the reminders fragments
  */
 class RemindersActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRemindersBinding
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_reminders)
-
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_reminders)
+        setSupportActionBar(binding.toolbar)
+        navController =
+            (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).findNavController()
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration)
+        if (FirebaseAuth.getInstance().currentUser == null) {
+            startSignInFlow()
+        }
     }
+
+    override fun onSupportNavigateUp(): Boolean = navController.navigateUp(appBarConfiguration)
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
-            android.R.id.home -> {
-                (nav_host_fragment as NavHostFragment).navController.popBackStack()
+            R.id.logout -> {
+                AuthUI.getInstance().signOut(this)
+                startSignInFlow()
                 return true
             }
         }
         return super.onOptionsItemSelected(item)
+    }
+
+    private fun startSignInFlow() {
+        startActivity(
+            Intent(
+                this,
+                AuthenticationActivity::class.java
+            )
+        )
+        finish()
     }
 }
